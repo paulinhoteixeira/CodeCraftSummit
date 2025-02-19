3b148faa5316dbe22213c83511ba7ae4da6c757a
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2025-02-19 19:49:17 -0300

    function to capture inputs

---
+++ main.js
@@ -0,0 +1,31 @@
+const app = document.getElementById("app");
+
+const formAction = () => {
+  const form = document.getElementById("form");
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form)
+    const userData = {
+      email: formData.get('email'),
+      phone: formData.get('phone'),
+    }
+  }
+}
+
+const startApp = () => {
+  const content = `
+    <form id="form">
+      <input type="email" name="email" placeholder="E-mail">
+      <input type="text" name="phone" placeholder="Telefone">
+      <button>
+        Confirmar
+      </button>
+    </form>
+  `
+
+  app.innerHTML = content
+
+  formAction()
+}
+
+startApp()
