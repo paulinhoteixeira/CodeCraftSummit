314ee432252aa19b1f0f507c0df38e970c66d45e
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2025-04-02 22:11:26 -0300

    forms added

--- main.js
+++ main.js
@@ -90,13 +90,45 @@ const updateImageLinks = () => {
 
 const startApp = () => {
   const content = `
-    <form id="form">
-      <input type="email" name="email" placeholder="E-mail">
-      <input type="text" name="phone" placeholder="Telefone">
-      <button>
-        Confirmar
-      </button>
-    </form>
+    <main>
+        <section class="about">
+          <div class="section-header">
+            <h2>Sobre o evento</h2>
+            <span class="badge">AO VIVO</span>
+
+            <p>Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackatons. <br/> <br/> Dias 15 a 17 de março | Das 18h às 21h | Online e Gratuito</p>
+          </div>
+        </section>
+
+        <section class="registration">
+          <h2>Inscrição</h2>
+
+          <form id="form">
+            <div class="input-wrapper">
+              <div class="input-group">
+                <label for="email">
+                  <img src="mail.svg" alt="Email icon">
+                </label>
+
+                <input type="email" id="email" name="email" placeholder="E-mail">
+              </div>
+
+              <div class="input-group">
+                <label for="phone">
+                  <img src="phone.svg" alt="Phone icon">
+                </label>
+
+                <input type="text" id="phone" name="phone" placeholder="Telefone">
+              </div>
+            </div>
+
+            <button>
+              Confirmar
+              <img src="arrow.svg" alt="Arrow right">
+            </button>
+          </form>
+        </section>
+      </main>
   `
 
   app.innerHTML = content
